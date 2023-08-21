81a29f9d3ba8ca82d317b2063e57a01c2beb1782
mangodm-web <mangodm.web3@gmail.com>
2023-08-21 16:30:10 +0900

    feat: 음성 인식 API 관련 라우터 추가

--- app/core/exception_handlers.py
+++ app/core/exception_handlers.py
@@ -1,31 +1,86 @@
+"""
+예외처리와 에러 응답을 생성하는 모듈
+"""
+
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from app.core.exceptions import CustomException
+from app.settings.constants import HTTP_STATUS_CODE, HTTP_STATUS_MESSAGE
 
 
-def custom_exception_handler(request: Request, exc: CustomException) -> JSONResponse:
+def create_error_response(
+    status_code: int, message: str, attribute: str, reason: str
+) -> JSONResponse:
     """
-    CustomException 인스턴스를 규격화된 에러 응답으로 변환한다.
+    규격화된 JSON 에러 응답을 생성한다.
 
     Args:
-        request (Request): FastAPI Request 객체
-        exc (CustomException): 처리할 CustomException 인스턴스
+        status_code (int): HTTP 응답 상태코드
+        message (str): HTTP 관련 메시지
+        attribute (str): 에러와 관련된 속성
+        reason (str): 에러 발생 사유
+
+    Returns:
+        JSONResponse: 에러 정보를 담고 있는 JSON 응답 객체
+    """
+
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "status": "error",
+            "code": status_code,
+            "message": message,
+            "details": {"attribute": attribute, "reason": reason},
+        },
+    )
+
+
+def custom_exception_handler(_: Request, exc: Exception) -> JSONResponse:
+    """
+    예외 케이스가 발생했을 경우, 규격화된 에러를 반환할 수 있도록 처리한다.
+
+    Args:
+        _ (Request): FastAPI Request 객체
+        exc (Exception): 처리할 Exception 인스턴스
 
     Returns:
         JSONResponse: 규격화된 에러 정보를 담고 있는 JSON 응답 객체
     """
 
-    error_response = {
-        "status": "error",
-        "code": exc.status_code,
-        "message": exc.message,
-        "details": {
-            "attribute": exc.attribute,
-            "reason": exc.reason,
-        },
-    }
+    if isinstance(exc, CustomException):
+        return create_error_response(
+            exc.status_code, exc.message, exc.attribute, exc.reason
+        )
 
-    return JSONResponse(
-        status_code=exc.status_code,
-        content=error_response,
+    if isinstance(exc, RequestValidationError):
+        field = ""
+        message = ""
+
+        for error in exc.errors():
+            if error["type"] == "missing":
+                input_field = error["input"]
+
+                if input_field is not None:
+                    field += error["loc"][1]
+                    message += f"The required field `{field}` is missing. Please check your request."
+                else:
+                    field += ""
+                    message += "The request body is empty. Please check your request."
+            if error["type"] == "string_type":
+                field += error["loc"][1]
+                message += f"The field `{field}` should be string type."
+
+        return create_error_response(
+            HTTP_STATUS_CODE["BAD_REQUEST"],
+            HTTP_STATUS_MESSAGE["BAD_REQUEST"],
+            field,
+            message,
+        )
+
+    return create_error_response(
+        HTTP_STATUS_CODE["INTERNAL_SERVER_ERROR"],
+        HTTP_STATUS_MESSAGE["INTERNAL_SERVER_ERROR"],
+        "internal",
+        "An internal error occurred while processing the request. Please try again later.",
     )
---
+++ app/routers/transcriptions.py
@@ -0,0 +1,98 @@
+"""
+음성 인식 관련 엔드포인트
+"""
+
+import base64
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+from google.cloud import speech_v2
+from google.api_core.exceptions import GoogleAPIError
+
+from app.api.models import TranscriptionCreate, TranscriptionResponse
+from app.core.exceptions import CustomException
+from app.api.utils import is_audio_content_valid
+
+from app.core.config import speech_recognition_config
+from app.settings.constants import *
+
+router = APIRouter(prefix=f"/api/{STT_API_VERSION_LATEST}")
+
+client = speech_v2.SpeechClient.from_service_account_file("app/settings/key.json")
+
+
+@router.post(
+    "/transcriptions",
+    response_model=TranscriptionResponse,
+    description="음성 인식 API",
+    tags=["transcriptions"],
+)
+async def create_transcription(audio: TranscriptionCreate) -> JSONResponse:
+    """
+    음성 데이터를 인식한 결과(텍스트)를 반환한다.
+
+    Args:
+        audio (TranscriptionCreate): 음성 인식을 하려는 파일을 base64로 인코딩 한 값
+
+    Returns:
+        JSONResponse: 음성 인식 결과
+    """
+
+    try:
+        if not is_audio_content_valid(audio.audio):
+            raise CustomException(
+                status_code=HTTP_STATUS_CODE["BAD_REQUEST"],
+                message=HTTP_STATUS_MESSAGE["BAD_REQUEST"],
+                attribute="audio",
+                reason="The audio data provided is not supported. Please send a valid audio.",
+            )
+
+        decoded_audio = base64.b64decode(audio.audio)
+
+        request = speech_v2.RecognizeRequest(
+            recognizer=f"projects/{GCP_PROJECT_ID}/locations/global/recognizers/_",
+            content=decoded_audio,
+            config=speech_recognition_config,
+        )
+
+        response = client.recognize(request=request)
+
+        if response.results:
+            result = response.results[0].alternatives[0]
+
+            [transcript, confidence] = [result.transcript, result.confidence]
+
+            if confidence < STT_API_CONFIDENCE_THRESHOLD:
+                raise CustomException(
+                    status_code=HTTP_STATUS_CODE["BAD_REQUEST"],
+                    message=HTTP_STATUS_MESSAGE["BAD_REQUEST"],
+                    attribute="audio",
+                    reason="The audio quality might be low. Please provide clearer audio.",
+                )
+
+            return JSONResponse(
+                status_code=HTTP_STATUS_CODE["OK"],
+                content={
+                    "status": "success",
+                    "code": HTTP_STATUS_CODE["OK"],
+                    "message": HTTP_STATUS_MESSAGE["OK"],
+                    "data": {"transcription": transcript},
+                },
+            )
+
+    except (CustomException, Exception) as exc:
+        if isinstance(exc, CustomException):
+            raise exc
+        if isinstance(exc, GoogleAPIError):
+            raise CustomException(
+                status_code=HTTP_STATUS_CODE["INTERNAL_SERVER_ERROR"],
+                message=HTTP_STATUS_MESSAGE["INTERNAL_SERVER_ERROR"],
+                attribute="external",
+                reason="An error occurred while communicating with other servers. Please try again later.",
+            ) from exc
+        raise CustomException(
+            status_code=HTTP_STATUS_CODE["INTERNAL_SERVER_ERROR"],
+            message=HTTP_STATUS_MESSAGE["INTERNAL_SERVER_ERROR"],
+            attribute="internal",
+            reason="An error occurred while processing the request. Please try again later.",
+        ) from exc
