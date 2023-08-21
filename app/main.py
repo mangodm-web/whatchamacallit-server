7455404a1a4ac545ac56dc734906f11d00c6ef44
mangodm-web <mangodm.web3@gmail.com>
2023-08-21 16:31:56 +0900

    feat: 어플리케이션 메인 모듈 변경: 라우터, 예외 처리 핸들러 함수 추가

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
+"""
+어플리케이션의 메인 모듈
+"""
+
 from fastapi import FastAPI
 
+from fastapi.exceptions import RequestValidationError
 from app.core.exceptions import CustomException
 from app.core.exception_handlers import custom_exception_handler
+
+from app.routers import transcriptions
 
 app = FastAPI()
 
 app.add_exception_handler(CustomException, custom_exception_handler)
+app.add_exception_handler(RequestValidationError, custom_exception_handler)
 
-
-@app.get("/")
-def index():
-    return "Hello world!"
+app.include_router(transcriptions.router)
