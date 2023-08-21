33ec9ba5e475757c6e761adb7b08c17fb11c7faf
mangodm-web <mangodm.web3@gmail.com>
2023-08-21 16:30:54 +0900

    feat: HTTP 응답 관련 코드, 메시지, GCP 프로젝트 ID 상수화

--- app/settings/constants.py
+++ app/settings/constants.py
@@ -1,3 +1,27 @@
+"""
+상수(constants) 변수 모음
+"""
+
+import os
+from dotenv import load_dotenv
+
+from fastapi import status
+
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+
 MODEL_API_VERSION_LATEST = "v1"
 STT_API_VERSION_LATEST = "v1"
 STT_API_CONFIDENCE_THRESHOLD = 0.5
+
+HTTP_STATUS_CODE = {
+    "OK": status.HTTP_200_OK,
+    "BAD_REQUEST": status.HTTP_400_BAD_REQUEST,
+    "INTERNAL_SERVER_ERROR": status.HTTP_500_INTERNAL_SERVER_ERROR,
+}
+HTTP_STATUS_MESSAGE = {
+    "OK": "OK",
+    "BAD_REQUEST": "Bad Request",
+    "INTERNAL_SERVER_ERROR": "Internal Server Error",
+}
