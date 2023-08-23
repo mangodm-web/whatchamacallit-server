a6c1f544d2608805b720ce96a17dc0ae8cc864e9
mangodm-web <mangodm.web3@gmail.com>
2023-08-23 21:33:36 +0900

    feat: 어플리케이션 메인 모듈 변경: 단어 추론 라우터 추가

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi.exceptions import RequestValidationError
 from app.core.exceptions import CustomException
 from app.core.exception_handlers import custom_exception_handler
 
-from app.routers import transcriptions
+from app.routers import transcriptions, predictions
 
 app = FastAPI()
 
@@ -16,3 +16,4 @@ app.add_exception_handler(CustomException, custom_exception_handler)
 app.add_exception_handler(RequestValidationError, custom_exception_handler)
 
 app.include_router(transcriptions.router)
+app.include_router(predictions.router)
