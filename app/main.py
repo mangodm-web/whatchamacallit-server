068c672f77faecaecadb4160148643323af48a9c
mangodm-web <mangodm.web3@gmail.com>
2023-08-24 01:02:06 +0900

    feat: 어플리케이션 메인 모듈 변경: 로깅 기능 추가

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@
 어플리케이션의 메인 모듈
 """
 
+import logging
+
 from fastapi import FastAPI
 
 from fastapi.exceptions import RequestValidationError
@@ -9,6 +11,8 @@ from app.core.exceptions import CustomException
 from app.core.exception_handlers import custom_exception_handler
 
 from app.routers import transcriptions, predictions
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
