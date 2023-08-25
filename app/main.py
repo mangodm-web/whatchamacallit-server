685507cb3e051caaf832ad2c53280b8967557bcf
mangodm-web <mangodm.web3@gmail.com>
2023-08-25 22:55:33 +0900

    feat: 어플리케이션 메인 모듈 변경: CORS 미들웨어 추가

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import logging
 
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.exceptions import CustomException
 from app.core.exception_handlers import custom_exception_handler
@@ -13,9 +14,19 @@ from app.core.exception_handlers import custom_exception_handler
 from app import global_config
 from app.routers import transcriptions, predictions
 
+from app.settings.constants import ALLOWED_ORIGINS
+
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ALLOWED_ORIGINS],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 app.add_exception_handler(CustomException, custom_exception_handler)
 app.add_exception_handler(RequestValidationError, custom_exception_handler)
