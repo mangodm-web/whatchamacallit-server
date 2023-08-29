7531b181b07ed4e0abcd833abf331b299489e812
mangodm-web <mangodm.web3@gmail.com>
2023-08-29 22:23:39 +0900

    feat: 단어 추론 피드백 관련 엔드포인트 추가

--- app/routers/predictions.py
+++ app/routers/predictions.py
@@ -2,16 +2,24 @@
 단어 추론 관련 엔드포인트
 """
 
+from datetime import datetime
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from app.api.models import PredictionCreate, PredictionResponse
+from app.api.models import (
+    PredictionCreate,
+    PredictionResponse,
+    FeedbackCreate,
+    FeedbackResponse,
+)
 from app.core.exceptions import CustomException
 from app.settings.constants import (
     HTTP_STATUS_CODE,
     HTTP_STATUS_MESSAGE,
     MODEL_API_VERSION_LATEST,
 )
+
+from app.core.mongodb_utils import get_mongodb_client, insert_document
 
 from app.services.model_inference import get_predictions
 
@@ -67,3 +75,75 @@ async def create_prediction(description: PredictionCreate) -> JSONResponse:
             attribute="model",
             reason="An error occurred while performing model inference. Please try again later.",
         ) from exc
+
+
+@router.post(
+    "/predictions/feedback",
+    response_model=FeedbackResponse,
+    summary="단어 추론 결과에 대한 피드백 API",
+    description="추론 결과에 대한 사용자의 피드백을 처리한다.",
+    tags=["predictions", "feedback"],
+)
+async def create_feedback(feedback: FeedbackCreate) -> JSONResponse:
+    """
+    단어 추론 결과에 대한 사용자의 피드백을 처리한다.
+
+    Args:
+        feedback (FeedbackCreate): 사용자가 제공한 피드백
+
+    Returns:
+        JSONResponse: 피드백 처리 결과
+    """
+
+    if not feedback:
+        raise CustomException(
+            status_code=HTTP_STATUS_CODE["BAD_REQUEST"],
+            message=HTTP_STATUS_MESSAGE["BAD_REQUEST"],
+            attribute="feedback",
+            reason="The provided feedback is not in the correct format. Please resend the data.",
+        )
+
+    try:
+        mongodb_client = get_mongodb_client()
+    except Exception as exc:
+        raise CustomException(
+            status_code=HTTP_STATUS_CODE["INTERNAL_SERVER_ERROR"],
+            message=HTTP_STATUS_MESSAGE["INTERNAL_SERVER_ERROR"],
+            attribute="mongodb",
+            reason="An internal error occurred while connecting to MongoDB. Please try again later.",
+        ) from exc
+
+    try:
+        created_id = insert_document(
+            mongodb_client,
+            "feedback_data",
+            {
+                "description": feedback.description,
+                "user_input": feedback.user_input,
+                "predictions": feedback.predictions,
+                "version_model": feedback.version_model,
+                "created_date": datetime.now(),
+                "correct_prediction_index": feedback.correct_prediction_index,
+            },
+        )
+
+        return JSONResponse(
+            status_code=HTTP_STATUS_CODE["CREATED"],
+            content={
+                "status": "success",
+                "code": HTTP_STATUS_CODE["CREATED"],
+                "message": HTTP_STATUS_MESSAGE["CREATED"],
+                "data": {"_id": created_id},
+            },
+        )
+    except CustomException as exc:
+        raise exc
+    except Exception as exc:
+        raise CustomException(
+            status_code=HTTP_STATUS_CODE["INTERNAL_SERVER_ERROR"],
+            message=HTTP_STATUS_MESSAGE["INTERNAL_SERVER_ERROR"],
+            attribute="general",
+            reason="An internal error occurred while processing your feeback. Please try again later.",
+        ) from exc
+    finally:
+        mongodb_client.close()
